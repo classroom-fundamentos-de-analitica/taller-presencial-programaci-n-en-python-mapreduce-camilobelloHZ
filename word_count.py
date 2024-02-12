b4a283bd8704277a86cc8ae884df361cd5a34e96
camilobelloHZ <cbelloh@unal.edu.co>
2024-02-12 11:11:32 -0500

    prueba 1

--- word_count.py
+++ word_count.py
@@ -13,9 +13,25 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
-def load_input(input_directory):
-    pass
+import glob
+import fileinput
 
+def load_input(input_directory):
+    """carga los archivos y genera la lista de tuplas"""
+    
+    filenames = glob.glob(input_directory + "/*.*")
+
+    sequence= []
+
+    with fileinput.input(files=filenames) as f:
+        for line in f:
+            sequence.append(
+                (f.filename(),line)
+            )
+
+    return sequence
+
+print(load_input("input/")[10])
 
 #
 # Escriba una función llamada maper que recibe una lista de tuplas de la
@@ -30,7 +46,8 @@ def load_input(input_directory):
 #   ]
 #
 def mapper(sequence):
-    pass
+    new_sequence= [(word.lower().replace(".","").replace(",",""), 1) for _, line in sequence for word in line.split() ]
+    return new_sequence
 
 
 #
@@ -39,31 +56,49 @@ def mapper(sequence):
 # clave.
 #
 #   [
-#     ('Analytics', 1),
+#     ('Analytics',# 1),
 #     ('Analytics', 1),
 #     ...
 #   ]
 #
 def shuffle_and_sort(sequence):
-    pass
+    sequence= sorted(
+        sequence,
+        key= lambda x: x[0]
+    )
+    return sequence
 
 
-#
 # Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
 # reduce los valores asociados a cada clave sumandolos. Como resultado, por
 # ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
 # texto.
-#
+
+from itertools import groupby
+
 def reducer(sequence):
-    pass
+    new_sequence=[]
+    for k,g in groupby(sequence, lambda x: x[0]):
+        key= k
+        value= sum(x[1] for x in g)
+        new_sequence.append(
+            (key,value)
+        )
+    return new_sequence
 
 
-#
+
+import os.path
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
 def create_ouptput_directory(output_directory):
-    pass
+    if os.path.isdir(output_directory):
+        raise Exception("el directorio ya existe")
+    os.mkdir(output_directory)
+
+
+
 
 
 #
@@ -75,7 +110,12 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    filename= os.path.join(output_directory,"part-00000")
+    with open(filename, "w") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
+
+
 
 
 #
@@ -83,18 +123,25 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
+    with open(os.path.join(output_directory, "_SUCESS"), "w") as f:
+        f.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input("input/")
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence= reducer(sequence)
+    create_ouptput_directory("output/")
+    save_output("output", sequence)
+    create_marker("output")
 
 
 if __name__ == "__main__":
-    job(
-        "input",
-        "output",
-    )
+     job(
+         "input",
+         "output",
+     )
